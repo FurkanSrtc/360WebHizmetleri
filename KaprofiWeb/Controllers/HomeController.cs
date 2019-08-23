1782860ee5bf450785d48c7c25c45420ca55d547
Furkan Sertçe <furkansertc@gmail.com>
2019-08-23 13:17:57 +0300

    k

---
+++ KaprofiWeb/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KaprofiWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [Route("hakkimizda")]
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+
+        [Route("hizmetlerimiz")]
+        public ActionResult Hizmetlerimiz()
+        {
+            return View();
+        }
+
+        [Route("referanslar")]
+        public ActionResult Referanslar()
+        {
+            return View();
+        }
+
+        public ActionResult WhyUs()
+        {
+            return View();
+        }
+
+        public ActionResult Team()
+        {
+            return View();
+        }
+
+        public ActionResult Clients()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ KaprofiWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KaprofiWeb.MvcApplication" Language="C#" %>
